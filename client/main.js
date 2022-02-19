d6584d59cbc5b4d42d86d7c93991653f10b350ce
Feddis08 <FuPuFsm3B>
2022-02-19 09:25:50 +0100

    added a preload images && added a sand_block

--- client/main.js
+++ client/main.js
@@ -1,6 +1,31 @@
 console.log("hello")
 var grid;
 var UUID;
+var images = [
+    "res/Blocks/stone_block.png",
+    "res/Blocks/grass_block.png",
+    "res/Blocks/water_block.png",
+    "res/Blocks/sand_block.png",
+    "res/Entities/Player/front.png",
+    "res/Entities/Player/back.png",
+    "res/Entities/Player/right.png",
+    "res/Entities/Player/left.png",
+];
+var image_counter = 0;
+update_loading_status_images = () => {
+    let status_bar = document.querySelector("#loading_status");
+    image_counter = image_counter + 1;
+    status_bar.innerHTML = "Loading images: " + image_counter + "/" + images.length;
+}
+const preloadImage = src =>
+    new Promise(r => {
+        const image = new Image()
+        image.onload = r
+        image.onerror = r
+        image.src = src
+        update_loading_status_images();
+    })
+
 moveByKeyCode = (key) => {
     if (key == "ArrowRight") grid.xPos++;
 }
@@ -8,7 +33,15 @@ function loadPage(href) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("GET", href, false);
     xmlhttp.send();
+
     return xmlhttp.responseText;
+}
+const loadPage2 = (href) => {
+    return fetch(href)
+        .then((response) => {
+            return response.text();
+        })
+
 }
 let Server = {
     grid: null,
@@ -60,7 +93,6 @@ let Server = {
             this.grid.update();
         })
         this.socket.on("self_player_update", (dataPacket) => {
-            //dataPacket = player
             let entities = [];
             entities.push(dataPacket.player)
             display_remove_entity(dataPacket.player.UUID);
@@ -68,7 +100,6 @@ let Server = {
             GameLoop.player = dataPacket.player;
         })
         this.socket.on("entity_move_update", (dataPacket) => {
-            //dataPacket = player
             let entities = [];
             entities.push(dataPacket.player)
             display_remove_entity(dataPacket.player.UUID);
@@ -83,11 +114,13 @@ let Server = {
             let chatOutput2 = document.querySelector("#chatOutput2");
             console.log(chatOutput2);
             chatOutput2.innerHTML = chatOutput2.innerHTML + "<br>" + dataPacket.message;
+            autoScroll();
         })
         this.socket.on("infoChat", (dataPacket) => {
             console.log(dataPacket.message);
             let chatOutput1 = document.querySelector("#chatOutput1");
             chatOutput1.innerHTML = chatOutput1.innerHTML + "<br>" + dataPacket.message;
+            autoScroll();
         })
         this.socket.on("watch_direction", (dataPacket) => {
             display_remove_entity(dataPacket.player.UUID);
@@ -115,19 +148,35 @@ let Server = {
         }, 1);
 
     },
-    startUp(dataPacket) {
+    sleep(ms = 2000) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    , startUp(dataPacket) {
         console.log(dataPacket);
 
         if (dataPacket.connect_allowed) {
-            document.body.innerHTML = loadPage("MultiPlayer/loggin.html");
-            const html_server_info = document.querySelector("#server_info");
-            const html_server_name = document.querySelector("#server_name");
-            const html_server_max_player = document.querySelector("#server_max_player");
-            const html_server_title = document.querySelector("#server_title");
-            html_server_info.innerHTML = `Info: ${dataPacket.server_info}`;
-            html_server_name.innerHTML = `Name: ${dataPacket.server_name}`;
-            html_server_max_player.innerHTML = `MAX_PLAYER: ${dataPacket.server_max_player}`;
-            html_server_title.innerHTML = `Title: ${dataPacket.server_title}`;
+            loadPage2("MultiPlayer/loggin.html").then(res => {
+                document.body.innerHTML = res;
+            })
+                .then(
+                    () => {
+                        Promise.all(images.map(x => preloadImage(x))).then(
+                            (x) => {
+                                let status_bar = document.querySelector("#loading_status");
+                                status_bar.innerHTML = "Loading images finished!"
+                            }
+                        );
+                        const html_server_info = document.querySelector("#server_info");
+                        const html_server_name = document.querySelector("#server_name");
+                        const html_server_max_player = document.querySelector("#server_max_player");
+                        const html_server_title = document.querySelector("#server_title");
+                        html_server_info.innerHTML = `Info: ${dataPacket.server_info}`;
+                        html_server_name.innerHTML = `Name: ${dataPacket.server_name}`;
+                        html_server_max_player.innerHTML = `MAX_PLAYER: ${dataPacket.server_max_player}`;
+                        html_server_title.innerHTML = `Title: ${dataPacket.server_title}`;
+
+                    }
+                )
 
         }
     },
@@ -161,4 +210,10 @@ goHome = () => {
     document.body.innerHTML = loadPage("index.html");
     Server.socket.close();
     Server.go_to_disc_page = true;
+}
+autoScroll = () => {
+    let chatOutput1 = document.querySelector("#chatOutput1");
+    chatOutput1.scrollTop = chatOutput1.scrollHeight;
+    let chatOutput2 = document.querySelector("#chatOutput2");
+    chatOutput2.scrollTop = chatOutput2.scrollHeight;
 }
